25ca5d8175f81118bd8d2b2634957b0a1c55d44b
cedrickummer <cedric.kummer@gmx.de>
2018-02-03 22:36:18 +0100

    Created beautiful LogIn screen

--- LogIn.php
+++ LogIn.php
@@ -11,7 +11,6 @@
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <link rel="stylesheet" href="css/main.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
 <body style="background-color:cornflowerblue;">
@@ -22,15 +21,53 @@
     <main class="mdl-layout__content">
         <div class="page-content"><!-- Your content goes here -->
             <div class="mdl-grid">
+                <div class="mdl-layout-spacer"></div>
+                <!-- Square card -->
+                <style>
+                    .demo-card-square.mdl-card {
+                        width: 320px;
+                        height: 320px;
+                    }
 
-                LogIn:
-                <input type="text" id="username">
-                <input type="password" id="password">
-                <input type="submit">
+                    .demo-card-square > .mdl-card__title {
+                        color: #fff;
+                        background: url('Bilder/Header_Bild.jpg') center 50% no-repeat #46B6AC;
+                    }
+                </style>
 
+                <div class="demo-card-square mdl-card mdl-shadow--2dp">
+                    <div class="mdl-card__title mdl-card--expand">
+                        <h2 class="mdl-card__title-text">LogIn</h2>
+                    </div>
+                    <!-- Textfield with Floating Label -->
+
+                    <form action="#">
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input" type="text" id="username">
+                            <label class="mdl-textfield__label" for="sample3">Benutzername</label>
+                        </div>
+                        <!-- Textfield with Floating Label -->
+
+                        <form action="#">
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" type="password" id="password">
+                                <label class="mdl-textfield__label" for="sample3">Passwort</label>
+                            </div>
+                        </form>
+
+                    </form>
+
+                    <div class="mdl-card__actions mdl-card--border">
+                        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                            Anmelden
+                        </a>
+                    </div>
+                </div>
+                </form>
+                <div class="mdl-layout-spacer"></div>
             </div>
             <?php
-            include "php-helper/footer.php";
+            include 'php-helper/footer.php';
             ?>
         </div>
     </main>
