5d77ae5ab69cfd99b3b9decabba689369fe0bac6
Norman <norman.rudo@gmail.com>
2018-02-03 21:18:23 +0100

    cleanup file

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html class="mdl-js">>
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <title>DPSG Windrose Anzing/Poing</title>
@@ -14,15 +14,19 @@
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   </head>
   <body style="background-color:cornflowerblue;">
-    <?php
-  include "php/header.php";
-  ?>
+		<div class="page-content">
+       <?php
+        include "php/header.php";
+      ?>
+      <!-- Your content goes here -->
+     		<div class="mdl-grid">
 
-<p> ich bin inhalt, der erste Inhalt den wir hier so haben </p>
+          <p> ich bin inhalt, der erste Inhalt den wir hier so haben </p>
 
-
-<?php
-  include 'php/footer.php';
- ?>
+			</div>
+      <?php
+        include 'php/footer.php';
+       ?>
+  	</div>
   </body>
 </html>
