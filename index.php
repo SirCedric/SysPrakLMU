38af7dd6a4d0fc60f54258226e070d69d52c5daa
cedrickummer <cedric.kummer@gmx.de>
2018-02-03 19:47:15 +0100

    Hintergrund jetzt cornflowerblue

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>meine erste Seite</title>
   </head>
-  <body style="background-color:blue;">
+  <body style="background-color:cornflowerblue;">
 
 hallo dies ist ein test
   einer html seite
