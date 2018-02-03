36fec84a5691d04180d4d9b163cc4b38fc78ed3f
cedrickummer <cedric.kummer@gmx.de>
2018-02-03 20:00:28 +0100

    In index: Benutzername- & Passwortfeld, sowie submit button

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <title>meine erste Seite</title>
   </head>
   <body style="background-color:cornflowerblue;">
-
 hallo dies ist ein test
   einer html seite
+  <input type="text" id="username">
+  <input type="password" id="password">
+  <input type="submit">
   </body>
 </html>
