5e799ca0ab8507bcc33ec481c7a40857c9eb501f
Norman <norman.rudo@gmail.com>
2018-02-03 20:59:31 +0100

    added Footer and clean up meta part of index.php

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html>
   <head>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-      <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-      <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <meta charset="utf-8">
-    <title>meine erste Seite</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <title>DPSG Windrose Anzing/Poing</title>
+
+        <link rel="shortcut icon" href="images/favicon.png">
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+        <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   </head>
-  <body style="background-color:cornflowerblue">
+  <body style="background-color:cornflowerblue;">
+    <?php
+  include "php/header.php";
+  ?>
+
+<p> ich bin inhalt, der erste Inhalt den wir hier so haben </p>
+
+
 <?php
-include "php/header.php";
-?>
+  include 'php/footer.php';
+ ?>
   </body>
 </html>
