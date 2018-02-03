05f51030edf7fbe5dfad3b539b968767346fbd9e
cedrickummer <cedric.kummer@gmx.de>
2018-02-03 21:45:45 +0100

    Formated code

--- LogIn.php
+++ LogIn.php
@@ -1,35 +1,35 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-  <title>DPSG Windrose Anzing/Poing</title>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title>DPSG Windrose Anzing/Poing</title>
 
-  <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="shortcut icon" href="images/favicon.png">
 
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
 <body style="background-color:cornflowerblue;">
-      <div class="mdl-layout mdl-js-layout">
-        <?php
-         include "php-helper/header.php";
-        ?>
-        <main class="mdl-layout__content">
-            <div class="page-content"><!-- Your content goes here -->
-              <div class="mdl-grid">
+<div class="mdl-layout mdl-js-layout">
+    <?php
+    include "php-helper/header.php";
+    ?>
+    <main class="mdl-layout__content">
+        <div class="page-content"><!-- Your content goes here -->
+            <div class="mdl-grid">
 
-LogIn:
-<input type="text" id="username">
-<input type="password" id="password">
-<input type="submit">
+                LogIn:
+                <input type="text" id="username">
+                <input type="password" id="password">
+                <input type="submit">
 
-</div>
-</div>
-</main>
+            </div>
+        </div>
+    </main>
 </div>
 
 </body>
--- php-pages/impressum.php
+++ php-pages/impressum.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html class="mdl-js">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
@@ -12,24 +12,24 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  </head>
-  <body style="background-color:cornflowerblue;">
-    		<div class="mdl-layout mdl-js-layout">
-          <?php
-           include "php-helper/header.php";
-          ?>
-    			<main class="mdl-layout__content">
-       				<div class="page-content"><!-- Your content goes here -->
-       					<div class="mdl-grid">
-<p> ich bin es, dein freundliches impressum </p>
-			</div>
-      <?php
-        include 'php-helper/footer.php';
-       ?>
-  	</div>
-  </main>
+</head>
+<body style="background-color:cornflowerblue;">
+<div class="mdl-layout mdl-js-layout">
+    <?php
+    include "php-helper/header.php";
+    ?>
+    <main class="mdl-layout__content">
+        <div class="page-content"><!-- Your content goes here -->
+            <div class="mdl-grid">
+                <p> ich bin es, dein freundliches impressum </p>
+            </div>
+            <?php
+            include 'php-helper/footer.php';
+            ?>
+        </div>
+    </main>
 </div>
 
 
-  </body>
+</body>
 </html>
