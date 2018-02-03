20da6bdfc48c3aa56a38c93d5f08eee9297ca736
Norman <norman.rudo@gmail.com>
2018-02-03 21:37:11 +0100

    fixed header navigation bobel

--- LogIn.php
+++ LogIn.php
@@ -1,22 +1,36 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+  <title>DPSG Windrose Anzing/Poing</title>
+
+  <link rel="shortcut icon" href="images/favicon.png">
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
-<body>
-<?php
-include "php/header.php";
-?>
+<body style="background-color:cornflowerblue;">
+      <div class="mdl-layout mdl-js-layout">
+        <?php
+         include "php/header.php";
+        ?>
+        <main class="mdl-layout__content">
+            <div class="page-content"><!-- Your content goes here -->
+              <div class="mdl-grid">
+
 LogIn:
 <input type="text" id="username">
 <input type="password" id="password">
 <input type="submit">
+
+</div>
+</div>
+</main>
+</div>
+
 </body>
 </html>
