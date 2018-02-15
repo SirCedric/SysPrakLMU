fc382dcee5f62c2534b857d372af996f3cef8d91
Norman <norman.rudo@gmail.com>
2018-02-15 12:09:04 +0100

    changed contac.php and added a "to try" part in header flyout

--- php-helper/header.php
+++ php-helper/header.php
@@ -34,6 +34,36 @@
     <span class="mdl-layout-title"><a class="header_button" href="index.php">DPSG Windrose</a></span>
 
     <nav class="mdl-navigation">
+        <div class="demo-card-square mdl-card header_login">
+          <div class="mdl-card__title mdl-card--expand">
+            <h2 class="mdl-card__title-text">Login</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label  <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                    <input type="text" name="username" class="mdl-textfield__input" value="<?php echo $username; ?>">
+                    <label class="mdl-textfield__label" for="username">Benutzername</label>
+                    <span class="help-block"><?php echo $username_err; ?></span>
+                </div>
+                <!-- Textfield with Floating Label -->
+
+
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                    <input class="mdl-textfield__input" name="password" type="password">
+                    <label class="mdl-textfield__label" for="sample3">Passwort</label>
+                    <span class="help-block"><?php echo $password_err; ?></span>
+                </div>
+                <p>Noch nicht registriert? </p>
+                <p><a href="register.php">Jetzt Account erstellen</a>.</p>
+                <div class="mdl-card__actions mdl-card--border">
+                    <input type="submit"
+                           class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect SingIn"
+                           value="Login">
+                </div>
+            </form>
+          </div>
+        </div>
+
         <a class="mdl-navigation__link" href="LogIn.php">LogIn</a>
         <a class="mdl-navigation__link" href="">Stufen</a>
         <a class="mdl-navigation__link" href="">Stamm</a>
