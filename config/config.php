c97cd8c902edcf566a0bd1545bba315a5a1a89e3
Norman <norman.rudo@gmail.com>
2018-02-23 19:28:31 +0100

    added config file, has to be configuerd...
    new folder for config files

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+// hier sollen alle datenbanken sachen eingeben werden, damit wir sie nicht an unterschiedlichen files aendern muesen wern noetig
+return array(
+    'host' => 'localhost',
+    'username' => 'root',
+);
+
+?>
