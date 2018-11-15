2758c4fa3ac1aea1de030c4982f654e0b3f2eada
SirCedric <cedric.kummer@gmx.de>
2018-11-15 16:05:01 +0100

    performConnection.c und config.h hinzugefügt

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define GAMEKINDNAME "Checkers"
+#define PORTNUMBER 1375
+#define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 
 int main(int argc, char** argv[]){
-  
+    
+    int sock = socket(gethostbyname(HOSTNAME), AF_INET, 0);
 }
---
+++ performConnection.c
@@ -0,0 +1,6 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
