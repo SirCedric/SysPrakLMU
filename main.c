1478adcafe3cc861891814e8dea46a4c2da3b91d
davidglsc <45068135+davidglsc@users.noreply.github.com>
2018-11-17 13:11:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,36 +4,46 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <errno.h>
 
 #define GAMEKINDNAME "Checkers"
 #define PORTNUMBER 1375
 #define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 
 int main(int argc, char* argv[]){
-    
+
     int sock = socket(gethostbyname(HOSTNAME), AF_INET, 0);
     long gameID;
     int playercount;
 
 
 // Verarbeitung der Kommandozeilenparameter
-    
-  if(argc != 5 || strcmp(argv[1], "-g") != 0 || strcmp(argv[3], "-p") != 0) {
-    printf("Error: Arguments\n");
+
+  if(argc != 5){
+    errno = 22;
+    perror("argc");
     return -1;
 }
-  
+
+  if(strcmp(argv[1], "-g") != 0 || strcmp(argv[3], "-p") != 0){
+    errno = 22;
+    perror("Flags");
+    return -1;
+  }
+
   gameID = atol(argv[2]);
   playercount = atoi(argv[4]);
 
   // "Falls gameID keine 13-stellige Zahl"
   if((double)gameID/1000000000000 < 1 || (double)gameID/9999999999999 > 1){
-    printf("Error: GameID\n");
+    errno = 22;
+    perror("GameID");
     return -1;
   }
 
   if(playercount != 1 && playercount != 2){
-    printf("Error: Playercount\n");
+    errno = 22;
+    perror("Playercount");
     return -1;
   }
 
