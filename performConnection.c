c60f7a15e33703d702d50d09ad18b68491bb46b5
SirCedric <cedric.kummer@gmx.de>
2018-11-23 12:04:31 +0100

    Prologphase in do while schleife gepackt

--- performConnection.c
+++ performConnection.c
@@ -10,8 +10,11 @@
 int performConnection(int socket){
     char buf[BUF_SIZE];
 
+
     ssize_t size;
     printf("Methode aufgerufen\n");
+
+    do{
     if((size = recv(socket, buf, BUF_SIZE, 0)) < 0){
         perror("ERROR recieving message!\n");
         return -1;
@@ -19,7 +22,7 @@ int performConnection(int socket){
     printf("%s\n", buf);
 
 
-    if (send(socket, "VERSION 2.3\n", BUF_SIZE, 0) < 0){
+    if (send(socket, "VERSION 2.0\n", BUF_SIZE, 0) < 0){
         perror("ERROR writing to socket");
         return -1;
         }
@@ -29,6 +32,21 @@ int performConnection(int socket){
         return -1;
     }
     printf("%s\n", buf);
+
+    if(send(socket, "ID 3n4hroe2iqxdx\n", BUF_SIZE, 0) < 0){
+        perror("ERROR sending message!\n");
+        return -1;
+    }
+
+    if((size = recv(socket, buf, BUF_SIZE, 0)) < 0 ){
+        perror("ERROR recieving message!\n");
+        return -1;
+    }
+    printf("%s\n", buf);
+
+    }while(strcmp(buf, "+ ENDPLAYERS") != 0);
+
+
 
 
     return 0;
