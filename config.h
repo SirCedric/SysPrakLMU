f4722be2224c6477676d928c9bc9cb51495ade82
Max-Leo Schmid <max-leo@MaxLeosckintosh.fritz.box>
2018-11-26 17:23:43 +0100

    parameters struct hinzugefügt

--- config.h
+++ config.h
@@ -1,1 +1,12 @@
+
+#define BUF_SIZE 500
+
+struct parameters{
+	char hostName[BUF_SIZE];
+	char portNr[BUF_SIZE];
+	char gameType[BUF_SIZE];
+};
+
+struct parameters getConfig(char filename[BUF_SIZE]);
+
 int performConnection(int Socket);
