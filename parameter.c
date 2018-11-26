6cce84221e60b08930bcff6fc7b701ff4e84a429
Max-Leo Schmid <max-leo@MaxLeosckintosh.fritz.box>
2018-11-26 17:20:04 +0100

    parameter.c aufgeräumt

--- parameter.c
+++ parameter.c
@@ -2,71 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER 256
-
-struct parameters{
-	char hostName[BUFFER];
-	char portNr[BUFFER];
-	char gameType[BUFFER];
-};
+#include "config.h"
 
 
 // returns struct parameter with parameters from specified config file
-struct parameters getConfig(char filename[BUFFER]){
+struct parameters getConfig(char filename[BUF_SIZE]){
 	
 	struct parameters result;
 	
 	FILE *config = fopen(filename, "r");
 		
-		char paraName[BUFFER];
-		char paraVal[BUFFER];
-		char lineBuffer[BUFFER];
-		
-		
+	char paraName[BUF_SIZE];
+	char paraVal[BUF_SIZE];
+	char lineBUF_SIZE[BUF_SIZE];
+	
+	
 
-		while (fgets(lineBuffer, BUFFER, config) != NULL) {
-			
-			sscanf(lineBuffer, "%s = %s", paraName, paraVal);
-			
-			if (lineBuffer[0] == '#'||(lineBuffer[0] == '/' && lineBuffer[1] == '/')) {
-				strcpy(paraName, "null");
-				printf("test\n");
-			}
-			
-			// reminder:
-			//	Wenn strings gleich sind gibt strcmp() 0 zurück!
-			if (strcmp(paraName, "hostname") == 0) {
-				strcpy(result.hostName, paraVal);
-//				printf("hostname: %s\n", result.hostName);
-			}
-			else if ((strcmp(paraName, "portnumber") == 0)) {
-				strcpy(result.portNr, paraVal);
-//				printf("portnumber: %i\n", result.portNr);
-			}
-			else if ((strcmp(paraName, "gametype") == 0)) {
-				strcpy(result.gameType, paraVal);
-//				printf("game type: %s\n", result.gameType);
-			}
-
+	while (fgets(lineBUF_SIZE, BUF_SIZE, config) != NULL) {
+		
+		sscanf(lineBUF_SIZE, "%s = %s", paraName, paraVal);
+		
+		if (lineBUF_SIZE[0] == '#'||(lineBUF_SIZE[0] == '/' && lineBUF_SIZE[1] == '/')) {
+			strcpy(paraName, "null");
+			printf("test\n");
+		}
+		
+		// reminder:
+		//	Wenn strings gleich sind gibt strcmp() 0 zurück!
+		if (strcmp(paraName, "hostname") == 0) {
+			strcpy(result.hostName, paraVal);
+		}
+		else if ((strcmp(paraName, "portnumber") == 0)) {
+			strcpy(result.portNr, paraVal);
+		}
+		else if ((strcmp(paraName, "gametype") == 0)) {
+			strcpy(result.gameType, paraVal);
 		}
 
-		fclose(config);
+	}
+
+	fclose(config);
 
 	
 	
 	return result;
 }
-
-
-/*
-// main function for testing
-int main() {
-	
-	
-	struct parameters config = getConfig("client.conf");
-	
-	printf("hostname: %s\nport number: %s\ngame type: %s", config.hostName, config.portNr, config.gameType);
-	
-	
-}
-*/
