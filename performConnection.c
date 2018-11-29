fd4446fa1fb046ac88f1fe57172a0d17f9ebd3ac
Max Schmid <maxleoschmid@me.com>
2018-11-29 17:27:00 +0100

    performConnection.c um eine printBoard Funktion erweitert.

--- performConnection.c
+++ performConnection.c
@@ -9,6 +9,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdbool.h>
 
 
 #include <netinet/in.h>
@@ -24,20 +25,63 @@ void sendToServer(int *socket, char message[BUF_SIZE]){
     
     if (write(*socket, buf, strlen(buf)) < 0) {
         perror("Fehler beim senden!");
+    } else {
+        printf("Client: %s", buf);
     }
-    printf("Client: %s", buf);
+    
 }
 
-char *readfromServer(int *socket){
+char *readfromServer(int *socket, bool print){
     
     memset(buf,0,BUF_SIZE);
     
     if (read(*socket, buf, BUF_SIZE) < 0) {
         perror("Fehler beim empfangen!");
+    } else {
+        strcpy(message, buf);
+
+        if(print == true)printf("Server: %s", buf);
     }
-    printf("Server: %s", buf);
-    strcpy(message, buf);
     return message;
+}
+
+void printBoard(char *board){
+    
+    char step1[500] = "";
+    char step2[500] = "";
+        
+    sscanf(board, "%*[^8,8]8,8\n%[^Y]s", step1);
+
+    for (int i = 0;i<500;i++) {
+        if (step1[i] == 'w' || step1[i] == '*' || step1[i] == 'b') {
+            char cToStr[2];
+            cToStr[0] = step1[i];
+            cToStr[1] = '\0';
+            strcat(step2, cToStr);
+        }
+    }
+    
+    // create board array
+    char boardArray[8][8];
+    int counter = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            boardArray[i][j] = step2[counter];
+            counter++;
+        }
+    }
+    
+    // print board array
+    printf(" +-----------------+\n");
+    for (int i = 0; i < 8; i++) {
+        printf("%i| ", (8-i));
+        for (int j = 0; j < 8; j++) {
+            printf("%c ", boardArray[i][j]);
+        }
+        printf("|\n");
+    }
+    printf(" +-----------------+\n");
+    printf("   A B C D E F G H\n");
 }
 
 
@@ -73,31 +117,31 @@ int performConnection(int *socket, char gameID[BUF_SIZE]){
 //    }
     
     // erste Nachricht empfangen
-    readfromServer(socket);
+    readfromServer(socket, true);
     
     // Versionsnummer schicken
     sendToServer(socket, "VERSION 2.0\n");
     
     // Antwort empfangen
-    readfromServer(socket);
-
+    readfromServer(socket, true);
     // Game ID senden
     sendToServer(socket, gameID);
 
     // Antwort empfangen
-    readfromServer(socket);
+    readfromServer(socket, true);
     
     // Antwort empfangen
-    readfromServer(socket);
+    readfromServer(socket, true);
     
     // Spieler senden
     sendToServer(socket, "PLAYER 0\n");
     
     // Antwort empfangen
-    readfromServer(socket);
+    readfromServer(socket, true);
     
-    // Antwort empfangen
-    readfromServer(socket);
+    
+    char *board = readfromServer(socket, false);
+    printBoard(board);
 
     return 0;
 }
